88b0a07ac22d4e303a4a9ea070f0c6642349020d
xaphan <grizafani@gmail.com>
2024-02-22 05:01:12 +0300

    constants changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'http://10.200.254.235:5000/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const NAME_MIN_LENGTH = 4;
